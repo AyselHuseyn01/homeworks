07ef4bba378f98553401d8e1f9274296108affe4
Aysel <167020526+AyselHuseyn01@users.noreply.github.com>
2024-11-01 17:29:02 +0400

    Add files via upload

---
+++ CarInventory.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class CarInventory {
+    public static void main(String[] args) {
+        String [][] cars=new String[][]{{"1", "2", "3"}, {"BMW", "Toyota", "Tesla"}};
+
+        displayGarage(cars);
+
+
+    }
+
+    // region DisplayGarage
+    public static void displayGarage(String[][] cars) {
+
+        for (int i = 0; i <= cars.length - 1; i++) {
+            for (int j = 0; j <= cars[i].length - 1; j++) {
+                System.out.print(cars[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    // endregion
+
+
+    public static void EntryCar(String space, String carName){
+        System.out.println("Add new car:");
+        Scanner scanner=new Scanner(System.in);
+
+
+    }
+}
